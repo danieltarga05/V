110070f9a55fd55eba0a54bcf4f446a0357f59bc
Andrea Giuliotti <andrea.giuliotti@iisviolamarchesini.edu.it>
2023-10-18 23:39:28 +0200

    Update esercizio test per l'utilizzo del metodo Delete()

--- informatica - php/progetto concerto/prova.php
+++ informatica - php/progetto concerto/prova.php
@@ -1,20 +1,21 @@
 <?php
 require 'concerto.php';
 
-$record = new Concerto('C01','concerto1','descrizione','2023/10/16');
+//$record = new Concerto('C01','concerto1','descrizione','2023/10/16');
+$record = new Concerto('C02','concerto2','descrizione2');
 $id_creato = Concerto::Create($record);
 echo "{$id_creato}\n";
 
 $trovato = Concerto::Find($record);
 echo "{$trovato}\n";
 
-/*if($record->__Delete())
+if($record->__Delete())
 {
     echo "bravo";
 }
 else
 {
     echo "male";
-}*/
+}
 
 ?>
