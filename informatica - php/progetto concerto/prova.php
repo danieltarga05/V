4ec8a12f0432027236967b288b743db156197553
Andrea Giuliotti <andrea.giuliotti@iisviolamarchesini.edu.it>
2023-10-22 18:55:30 +0200

    Update file di test

--- informatica - php/progetto concerto/prova.php
+++ informatica - php/progetto concerto/prova.php
@@ -2,17 +2,20 @@
 require 'concerto.php';
 
 //$record = new Concerto('C01','concerto1','descrizione','2023/10/16');
-$record = new Concerto('C02', 'concerto2', 'descrizione2');
+/*$record = new Concerto('C02', 'concerto2', 'descrizione2');
 $array = [
     'codice'=> $record->__Get_Codice(),
-    'titolo'=> $record->__Get_Codice(),
-    'descrizione'=> $record->__Get_Codice(),
+    'titolo'=> $record->__Get_Titolo(),
+    'descrizione'=> $record->__Get_Descrizione(),
     'data_concerto'=> $record->__Get_Data_Concerto()
 ];
 $id_creato = Concerto::Create($array);
-echo "{$id_creato}\n";
+echo "{$id_creato}\n";*/
 
-
+$concerti = Concerto::FindAll();
+foreach ($concerti as $a) {
+    echo $a->__Get_Codice() . ' ' . $a->__Get_Titolo() . ' ' . $a->__Get_Data_Concerto()->format('Y m d') . PHP_EOL;
+}
 
 /*$concerti = Concerto::FindAll();
 foreach($concerto as $concerti)
